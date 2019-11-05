90cc5f846d6483e53b51eecce3d0ebdd4da0461e
Aaron victor winnercoz <Winnercoz340@gmail.com>
2019-11-04 21:46:19 -0800

    Completed module

---
+++ js/ratings.js
@@ -0,0 +1,22 @@
+function collect_rating(){
+const ratings = { count: 0,sum : 0,average : 0
+};
+
+let rating = 0;
+const elements = document.querySelectorAll('.rating');
+elements.forEach(element => {
+rating = parseInt(element.id.replace('star', ''));
+ratings.count += parseInt(element.value);
+ratings.sum += parseInt(element.value) * ratings;
+});
+if( ratings.count !== 0){
+    ratings.average = rating.sum / ratings,count;
+}
+
+return ratings;
+}
+
+document.addEventListener('change', () => {
+    const ratings = collect_rating;
+    document.querySelectorAll('#average').value = ratings.average.toFixed(2);
+})
